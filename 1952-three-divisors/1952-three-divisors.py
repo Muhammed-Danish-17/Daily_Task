6082f59f8959a34eef69e6a898a302037e9eb608
Muhammed-Danish-17 <muhddanishak17@gmail.com>
2026-05-02 15:15:13 +0530

    Time: 4 ms (48.65%), Space: 12.6 MB (59.46%) - LeetHub

---
+++ 1952-three-divisors/1952-three-divisors.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def isThree(self, n):
+        count=0
+        for i in range(1,n+1):
+            if n % i==0:
+                count+=1
+        return count==3
+        
